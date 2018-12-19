7f4ce6fd44fa9ff14cb4506cce9cce01a94d957e
Austin <joha0033@gmail.com>
2018-12-19 16:41:56 -0700

    added redux store

--- src/getStore.js
+++ src/getStore.js
@@ -1,6 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { identity } from 'lodash'
 
-export default function (defaultState ={ test: 'TESTER'}) {
+export default function (defaultState = { 
+    test: 'sick...'
+}) {
     const store = createStore(identity, defaultState)
+    return store
 }
