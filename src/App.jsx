1b0822bca7e145d592a3bb6db72e0f823e45bac5
Austin <joha0033@gmail.com>
2018-12-19 16:09:38 -0700

    added basic react render fn

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+const AppDisplay = ( ) => {
+    <div>
+        <h1>
+            ISO REACT, ISO COOL
+        </h1>
+    </div>
+}
+
+export default AppDisplay
