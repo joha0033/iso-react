fc7d0600ead71e4da771a7dd805dd4fa59f450a8
Austin <joha0033@gmail.com>
2018-12-19 16:10:07 -0700

    added basic react render fn

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,12 @@
-console.log('react is pretty swell');
+import App from './App'
+import ReactDom from 'React-dom'
+import React from 'react';
+
+const render = (_App) => {
+    ReactDOM.render(
+        <_App/>,
+        document.getElementById('AppContainer')
+    )
+}
+
+render(App)
